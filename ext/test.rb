4e6d9129171bf02940643df9ef0b6b70c091a016
Emil Palm <emil@x86.nu>
2011-04-08 18:26:31 +0200

    This actually works now

--- ext/test.rb
+++ ext/test.rb
@@ -1,2 +1,2 @@
-$:.unshift "#{File.dirname(__FILE__)}";require "segmenter_ext";a = Segmenter::segment("/Users/emil/code/ruby-av-segmenter/ext/public/video/09a7ca73-9ee6-4664-b73a-cc342c556fba/09a7ca73-9ee6-4664-b73a-cc342c556fba","09a7ca73-9ee6-4664-b73a-cc342c556fba",40);b = a[0]; puts "#{b.index} - #{b.duration} - #{b.filename}\n"
+$:.unshift "#{File.dirname(__FILE__)}";require "segmenter_ext";a = Segmenter::segment("oi.ts","najs",10);b = a[0]; puts "#{b.index} - #{b.duration} - #{b.filename}\n"; puts a
 #$:.unshift "#{File.dirname(__FILE__)}";require "segmenter_ext";a = Segmenter::segment("bajs/bajs.ts","09a7ca73-9ee6-4664-b73a-cc342c556fba",40);b = a[0]; puts "#{b.index} - #{b.duration} - #{b.filename}\n"
